3682aba63204d7c0cdcd9f49a697cc36ffa8e998
XeniyaZ <kseniya10081989@gmail.com>
2023-07-17 09:34:51 -0500

    crete loader spinner react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import Accordion from "./Accordion/Accordion";
 import "./App.css";
 import Tabs from "./Tabs/Tabs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TabInfoOne from "./Tabs/TabInfoOne";
 import TabInfoThree from "./Tabs/TabInfoThree";
 import TabInfoTwo from "./Tabs/TabInfoTwo";
 import Calc from "./Calculator/Calc";
 import Content from "./Modal/Content";
 import Modal from "./Modal/Modal";
+import LoaderPage from "./Loader/LoaderPage";
 
 function App() {
+
+  // TABS
   const [activeTab, setActiveTab] = useState(0);
 
+
+  // MODAL
   const [isOpen, setIsOpen] = useState(false);
 
+  // LOADER
+
+  const[stateLoader, setStateLoader] = useState(true);
+  useEffect (() => {
+    const timer = setTimeout(() => setStateLoader(false, 3000));
+    return () => clearTimeout(timer);
+  }, [])
+
+  // https://mhnpd.github.io/react-loader-spinner/docs/components/ball-traingle
+
+
   return (
+    
     <div>
       <Accordion />
 
@@ -25,13 +42,19 @@ function App() {
       {activeTab === 1 && <TabInfoTwo />}
       {activeTab === 2 && <TabInfoThree />}
 
+    {/* MODAL */}
     <button onClick={()=> setIsOpen(true)}>Open Modal</button>
 
       {isOpen &&
       <Modal setIsOpen={setIsOpen}>
       <Content setIsOpen={setIsOpen} />
       </Modal>
-      }
+      } 
+
+      {/* LOADER */}
+
+      { stateLoader && <LoaderPage />}
+
        </div>
   );
 }
---
+++ src/Loader/Loader.js
@@ -0,0 +1,18 @@
+import './loader.css';
+
+const Loader = () =>{
+    return(
+        <div className="lds_roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Loader;
